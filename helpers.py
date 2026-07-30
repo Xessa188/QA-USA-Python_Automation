499db4f4e2068b5131c5e9e709bdae9df9e45f30
Xessa <Basementzcracked7@gmail.com>
2026-07-30 12:38:06 -0500

    Fix helpers template and add print statements to test functions

--- helpers.py
+++ helpers.py
@@ -1,16 +1,12 @@
+import urllib.request
+
+
 def is_url_reachable(url):
-    import urllib.request
-    import ssl
-
     try:
-        ssl_ctx = ssl.create_default_context()
-        ssl_ctx.check_hostname = False
-        ssl_ctx.verify_mode = ssl.CERT_NONE
-
-        with urllib.request.urlopen(url, context=ssl_ctx) as response:
-            if response.status == 200:
-                return True
-            else:
-                return False
-    except Exception as e:
+        response = urllib.request.urlopen(url)
+        if response.status == 200:
+            return True
+        else:
+            return False
+    except Exception:
         return False
